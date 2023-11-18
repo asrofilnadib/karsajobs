af67a24f1489da75189e4c8b16d91bd758223536
Asrofil Nadib <asrofilnadibs28@gmail.com>
2023-11-18 10:54:47 +0700

    Initial message commit

---
+++ karsajobs/build_push_image_karsajobs.sh
@@ -0,0 +1,17 @@
+IMAGE="asrofilnadib/karsajobs:latest"
+
+echo "Membuat docker images..."
+# Perintah untuk membuat Docker image dari Dockerfile
+docker build -t $IMAGE .
+
+echo "Mengubah nama sesuai dengan format docker registry"
+# Mengubah nama image agar sesuai dengan format Docker Hub (atau GitHub Packages)
+docker tag $IMAGE ghcr.io/$IMAGE
+
+echo "Login ke Github Packages"
+# Login ke Docker Hub (atau GitHub Packages)
+echo "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" | docker login ghcr.io -u asrofilnadib --password-stdin
+
+echo "Mengunggah image ke Github Packages"
+# Mengunggah image ke Docker Hub (atau GitHub Packages)
+docker push ghcr.io/$IMAGE
